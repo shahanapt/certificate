3961f0a69c0e4890b6bfa73c37d0a42a3b73b7b1
shahana pt <fshahanapt@gmail.com>
2023-04-14 11:46:18 +0530

    second committ

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,41 +1,48 @@
-import React from 'react'
-import './Banner.css'
-import {Row,Col,Container,Button} from 'react-bootstrap'
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import "./Banner.css";
+import { Row, Col, Container, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
   return (
-    <div className='banner'>
-        <Container>
+    <div className="banner">
+      <Container>
         <Row my-auto>
-            <Col md-6>
+          <div className="d-flex main-flex">
+            {/* <Col md-6> */}
+            <div className="banner-left">
+              <h1 style={{ textAlign: "left" }}>
+                Make Your Career<br></br>
+                With EdisonValley
+              </h1>
+              <p className="banpara">
+                We are a bunch of tech enthusiasts determined to<br></br>
+                uplift the society the latest technologies around us.
+              </p>
+              <Link to="/login">
+                <Button className="text-white banbutton" variant="warning">
+                  Login
+                </Button>
+              </Link>
+            </div>
+            {/* </Col> */}
 
-                <div className='banner-left'>
-                    <h1 style={{textAlign: 'left'}}>Make Your Career<br></br>
-                        With EdisonValley</h1>
-                    <p className='banpara'>We are a bunch of tech enthusiasts determined to<br></br>uplift the society the latest technologies around us.
-                    </p>
-                    <Link to='/login'><Button className="text-white banbutton" variant="warning" >Login</Button></Link>
-                </div>
+            {/* <Col md-6> */}
 
+            <div className="banner-right">
+              <img
+                className="banner-image"
+                src={require("./sample.jpg")}
+                alt="error"
+              />
+            </div>
 
-            </Col>
-
-            <Col md-6>
-
-                <div className='banner-right'>
-                    <img src={require("./sample.jpg")} alt="error" />
-                </div>
-                
-
-            </Col>
+            {/* </Col> */}
+          </div>
         </Row>
-        </Container>
-
-        
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
